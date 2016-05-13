52e6f4d5942fae0cca1eb5cf44c28490f8a596eb
Kouhei Maeda <mkouhei@palmtb.net>
2016-05-14 05:43:23 +0900

    Adds license categories.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- lic_check/__init__.py
+++ lic_check/__init__.py
@@ -1,1 +1,25 @@
 """lic_check."""
+
+# based on https://www.gnu.org/licenses/license-list.en.html
+
+
+SEGMENTS = {'SoftwareLicenses': 'Software Licenses',
+            'DocumentationLicenses': 'Licenses For Documentation',
+            'OtherWorksLicenses': 'Licenses for Other Works'}
+
+SOFTWARE_LICENSES = {
+    'GPLCompatibleLicenses': 'GPL-Compatible Free Software Licenses',
+    'GPLIncompatibleLicenses': 'GPL-Incompatible Free Software Licenses',
+    'NonFreeSoftwareLicenses': 'Nonfree Software Licenses'}
+
+DOCUMENTATION_LICENSES = {
+    'FreeDocumentationLicenses': 'Free Documentation Licenses',
+    'NonFreeDocumentationLicenses': 'Nonfree Documentation Licenses'}
+
+OTHER_WORKS_LICENSES = {
+    'OtherLicenses': ('Licenses for Works of Practical Use '
+                      'besides Software and Documentation'),
+    'Fonts': 'Licenses for Fonts',
+    'OpinionLicenses': ('Licenses for Works stating a Viewpoint'
+                        '(e.g., Opinion or Testimony)'),
+    'Designs': 'Licenses for Designs for Physical Objects'}
