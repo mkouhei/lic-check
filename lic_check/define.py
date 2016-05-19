b241c3e9f43343e26ed8f9d350398dc8180ff9eb
Kouhei Maeda <mkouhei@palmtb.net>
2016-05-19 16:44:21 +0900

    Adds licenses method.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- lic_check/define.py
+++ lic_check/define.py
@@ -40,3 +40,28 @@ class License(object):
                 .filter(lambda i, this: PyQuery(this)
                         .attr('href') == '#{0}'.format(segment))
                 .siblings('ul').find('a')]
+
+    def licenses(self, category):
+        """licenses.
+
+        >>> with open('lic_check/license.html') as f:
+        ...     data = f.read()
+        >>> l = License(data)
+        >>> l.segments()[0]
+        'SoftwareLicenses'
+        >>> l.categories(l.segments()[0])[0]
+        'GPLCompatibleLicenses'
+        >>> len(l.licenses(l.categories(l.segments()[0])[0]))
+        59
+        >>> l.categories(l.segments()[0])[1]
+        'GPLIncompatibleLicenses'
+        >>> len(l.licenses(l.categories(l.segments()[0])[1]))
+        40
+        >>> len(l.licenses(l.categories(l.segments()[0])[2]))
+        35
+        """
+        return [i.get('id')
+                for i in (self.html
+                          .find('.big-subsection h4#{0}'.format(category))
+                          .parent().next_all('dl').eq(0).children('dt a'))
+                if i.get('id')]
